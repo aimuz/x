31dd637f1320f6aa8f5c78d0d2f7939d02b1d687
aimuz <mr.imuz@gmail.com>
2021-10-23 15:32:16 +0800

    ✨ spa server

--- http/fs.go
+++ http/fs.go
@@ -5,23 +5,24 @@ import (
 	"path"
 )
 
-type FileServer struct {
+type fileServer struct {
 	prefix string
 	fs     http.FileSystem
 }
 
-// PrefixFileServer return a Handler
-func PrefixFileServer(prefix string, fs http.FileSystem) http.Handler {
-	return http.FileServer(&FileServer{
+func PrefixFileSystem(prefix string, fs http.FileSystem) http.FileSystem {
+	return &fileServer{
 		prefix: prefix,
 		fs:     fs,
-	})
+	}
 }
 
-func (f *FileServer) Open(name string) (http.File, error) {
+// PrefixFileServer return a Handler
+func PrefixFileServer(prefix string, fs http.FileSystem) http.Handler {
+	return http.FileServer(PrefixFileSystem(prefix, fs))
+}
+
+func (f *fileServer) Open(name string) (http.File, error) {
 	name = path.Join(f.prefix, name)
 	return f.fs.Open(name)
-}
-
-type SPAServer struct {
 }
---
+++ http/spa.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type spaHandler struct {
+	index string
+	fs    http.FileSystem
+}
+
+func SPAServer(index string, fs http.FileSystem) http.Handler {
+	return &spaHandler{
+		index: index,
+		fs:    fs,
+	}
+}
+
+func (s *spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// check whether a file exists at the given path
+	_, err = s.fs.Open(path)
+	if os.IsNotExist(err) {
+		if !strings.HasPrefix(s.index, "/") {
+			s.index = "/" + s.index
+		}
+		r.URL.Path = s.index
+		http.FileServer(s.fs).ServeHTTP(w, r)
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(s.fs).ServeHTTP(w, r)
+}
